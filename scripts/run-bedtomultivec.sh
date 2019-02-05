f9a3eb6b1b24259b9c413785424a8d5f5db1e802
Lmercadom <42242797+Lmercadom@users.noreply.github.com>
2019-02-05 12:18:42 -0500

    Create run-bedtomultivec.sh

---
+++ scripts/run-bedtomultivec.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Converts a bed file in a 'states' format to multivec
+
+INPUT=$1
+ASSEMBLY=$2
+RESOLUTION=$3
+ROW_INFOS_FILE=$4
+NUM_ROWS=$5
+FORMAT=$6
+OUTDIR=$2
+
+FILE_BASE=$(basename $INPUT)
+FILE_NAME=${FILE_BASE%.*}
+echo $FILE_NAME
+
+if [ ! -d "$OUTDIR" ]
+then
+  mkdir $OUTDIR
+fi
+
+outputfile = "$OUTDIR/$FILE_NAME.multires.mv5"
+
+clodius convert bedfile_to_multivec \
+    $INPUT \
+    --output-file $OUTDIR
+    --assembly $ASSEMBLY \
+    --starting-resolution $RESOLUTION \
+    --num-rows $NUM_ROWS \
+    --format states
+    --row_infos-filename $ROW_INFOS_FILE
