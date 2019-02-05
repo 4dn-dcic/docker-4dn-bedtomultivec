121a235a89bb0c0f0e7a6679641a163a1bf78243
Lmercadom <42242797+Lmercadom@users.noreply.github.com>
2019-02-05 11:54:06 -0500

    Update downloads.sh

--- downloads.sh
+++ downloads.sh
@@ -1,5 +1,9 @@
 ## SOFTWARE: clodius
-## VERSION: 0.9.4
+## VERSION: 0.10.2 (Under Development)#commit:c39d5204a59726154b5a35218fe0242f3fb93d86
 ## TYPE: Tile generator for big data
 ## SOURCE URL: https://github.com/higlass/clodius
-pip install clodius==0.9.4
+git clone https://github.com/higlass/clodius
+cd clodius/
+git checkout develop
+make
+cd ..
